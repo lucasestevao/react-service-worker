93518404d31a3a13cb50b728cb71f8d5a325f674
Lucas Estevão de Souza <lestevaodesouza@apple.com>
2019-11-01 14:13:20 -0700

    Building project.

---
+++ build/precache-manifest.93ee791fec80d4d2e257846e08d89e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9d41d8b97f7add989e27976a669d4f",
+    "url": "/react-service-worker/index.html"
+  },
+  {
+    "revision": "d66ffb4db6d840559210",
+    "url": "/react-service-worker/static/css/main.3b25a1a2.chunk.css"
+  },
+  {
+    "revision": "9285c3cc4e469fa1dbec",
+    "url": "/react-service-worker/static/js/2.efa51d18.chunk.js"
+  },
+  {
+    "revision": "d66ffb4db6d840559210",
+    "url": "/react-service-worker/static/js/main.90eb3f6b.chunk.js"
+  },
+  {
+    "revision": "a60c38aa918db0dc8634",
+    "url": "/react-service-worker/static/js/runtime~main.e9ed2561.js"
+  }
+]);
